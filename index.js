8348bb5dbb979c4b8a8e1b7bb572eaa5820d75e0
Faizakarimi <38805945+Faizakarimi@users.noreply.github.com>
2024-04-29 22:08:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const number1 = 0;
 
 
-fetch('GET https://www.thecolorapi.com/')
+fetch('GET https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html&mode=analogic&count=6')
     .then(res =>{
 
         return res.json()

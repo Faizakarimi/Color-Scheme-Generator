9abf1ca4b4e1ae53da42fb22177afac362c1adcf
Faizakarimi <38805945+Faizakarimi@users.noreply.github.com>
2024-05-02 11:56:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,41 @@ const colorContainer = document.getElementById('colors-container')
 const colorPicker = document.getElementById('color-picker')
 const colorModeList = document.getElementById('colorsMenu')
 let defaultColor = colorPicker.value.substring(1)
-
+let Chosencolor
 let mode = ''
 let r = ''
 let g = ''
 let b = ''
 // colorMode.options[colorMode.selectedIndex].text
-// console.log(defaultColor)
+console.log(defaultColor)
 
+function getDefaultColorScheme(){
+    console.log(defaultColor)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${defaultColor}&rgb=&{r},&{g},&{b}&mode=${mode}`)
+    .then(res => res.json())
+    .then(data => {
+        const colorsArray = data.colors
+        console.log(colorsArray)
+        colorsArray.map(color => {
+            colorContainer.innerHTML += ` <div class="color-image"><img src = ${color.image.bare}> 
+        <p class="color-value" data-color = ${color.hex.value}>${color.hex.value}</p></div>`
+
+        })
+
+    })
+}
+
+getDefaultColorScheme()
 
 colorModeList.addEventListener('change', (e) => {
     mode = e.target.options[colorModeList.selectedIndex].text.toLowerCase()
     // console.log(mode)
 })
 colorPicker.addEventListener('change', function (e) {
-    defaultColor = e.target.value.substring(1);
-    r = parseInt(defaultColor.substr(1, 2), 16)
-    g = parseInt(defaultColor.substr(3, 2), 16)
-    b = parseInt(defaultColor.substr(5, 2), 16)
+    Chosencolor = e.target.value.substring(1);
+    r = parseInt(Chosencolor.substr(1, 2), 16)
+    g = parseInt(Chosencolor.substr(3, 2), 16)
+    b = parseInt(Chosencolor.substr(5, 2), 16)
     // console.log(defaultColor)
     // console.log('e:' + color)
     // console.log(`${r} +${g} +${b}`)
@@ -32,7 +49,7 @@ colorPicker.addEventListener('change', function (e) {
 function getColorScheme() {
     // console.log(defaultColor)
     let html = ``
-    fetch(`https://www.thecolorapi.com/scheme?hex=${defaultColor}&rgb=&{r},&{g},&{b}&mode=${mode}`)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${Chosencolor}&rgb=&{r},&{g},&{b}&mode=${mode}`)
         .then(res => res.json())
         .then(data => {
             const colorsArray = data.colors

a5347159a16b2e5e7a935ee45837589effbab4c1
Faizakarimi <38805945+Faizakarimi@users.noreply.github.com>
2024-05-02 11:41:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,68 @@
 
-const number1 = 0;
+const colorContainer = document.getElementById('colors-container')
+const colorPicker = document.getElementById('color-picker')
+const colorModeList = document.getElementById('colorsMenu')
+let defaultColor = colorPicker.value.substring(1)
+
+let mode = ''
+let r = ''
+let g = ''
+let b = ''
+// colorMode.options[colorMode.selectedIndex].text
+console.log(defaultColor)
 
 
-fetch('GET https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&format=html&mode=analogic&count=6')
-    .then(res =>{
+colorModeList.addEventListener('change', (e) => {
+    mode = e.target.options[colorModeList.selectedIndex].text.toLowerCase()
+    console.log(mode)
+})
+colorPicker.addEventListener('change', function (e) {
+    defaultColor = e.target.value.substring(1);
+    r = parseInt(defaultColor.substr(1, 2), 16)
+    g = parseInt(defaultColor.substr(3, 2), 16)
+    b = parseInt(defaultColor.substr(5, 2), 16)
+    console.log(defaultColor)
+    // console.log('e:' + color)
+    console.log(`${r} +${g} +${b}`)
+})
 
-        return res.json()
-    }  )
-    .then(data => console.log(data))
+
+
+
+function getColorScheme() {
+    // console.log(defaultColor)
+    let html = ``
+    fetch(`https://www.thecolorapi.com/scheme?hex=${defaultColor}&rgb=&{r},&{g},&{b}&mode=${mode}`)
+        .then(res => res.json())
+        .then(data => {
+            const colorsArray = data.colors
+            console.log(colorsArray)
+            colorsArray.map(color => {
+                colorContainer.innerHTML += ` <div class="color-image"><img src = ${color.image.bare}> 
+            <p class="color-value" data-color = ${color.hex.value}>${color.hex.value}</p></div>`
+
+            })
+
+        })
+    colorContainer.innerHTML = ''
+
+
+}
+
+document.getElementById('get-btn').addEventListener('click', getColorScheme)
+// colorContainer.innerHTML = getColorScheme()
+
+document.addEventListener('click', function (e) {
+     if(!navigator.clipboard){
+        console.log('error')
+     }
+    else if(e.target.dataset.color) {
+        Navigator.clipboard.writeText(e.target.dataset.color)
+        alert(`e.target.dataset.color copied to clipboard`)
+        console.log(e.target.dataset.color)
+    }
+
+})
+
+
+

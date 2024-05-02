dab8ef1615329fb980efc80c4940dc490eb09add
Faizakarimi <38805945+Faizakarimi@users.noreply.github.com>
2024-05-02 11:56:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ let b = ''
 // colorMode.options[colorMode.selectedIndex].text
 console.log(defaultColor)
 
-function getDefaultColorScheme(){
+function getDefaultColorScheme() {
     console.log(defaultColor)
     fetch(`https://www.thecolorapi.com/scheme?hex=${defaultColor}&rgb=&{r},&{g},&{b}&mode=${mode}`)
-    .then(res => res.json())
-    .then(data => {
-        const colorsArray = data.colors
-        console.log(colorsArray)
-        colorsArray.map(color => {
-            colorContainer.innerHTML += ` <div class="color-image"><img src = ${color.image.bare}> 
+        .then(res => res.json())
+        .then(data => {
+            const colorsArray = data.colors
+            console.log(colorsArray)
+            colorsArray.map(color => {
+                colorContainer.innerHTML += ` <div class="color-image"><img src = ${color.image.bare}> 
         <p class="color-value" data-color = ${color.hex.value}>${color.hex.value}</p></div>`
 
-        })
+            })
 
-    })
+        })
 }
 
 getDefaultColorScheme()
@@ -47,8 +47,6 @@ colorPicker.addEventListener('change', function (e) {
 
 
 function getColorScheme() {
-    // console.log(defaultColor)
-    let html = ``
     fetch(`https://www.thecolorapi.com/scheme?hex=${Chosencolor}&rgb=&{r},&{g},&{b}&mode=${mode}`)
         .then(res => res.json())
         .then(data => {
@@ -67,10 +65,9 @@ function getColorScheme() {
 }
 
 document.getElementById('get-btn').addEventListener('click', getColorScheme)
-// colorContainer.innerHTML = getColorScheme()
 
 document.addEventListener('click', function (e) {
-   if(e.target.dataset.color) {
+    if (e.target.dataset.color) {
         Navigator.clipboard.writeText(e.target.dataset.color)
         alert(`e.target.dataset.color copied to clipboard`)
         // console.log(e.target.dataset.color)
